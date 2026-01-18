0c6fa5a440fead020b800f161a55241267f0cf96
GustoVasconcelos <augusto@lothuscorp.com.br>
2026-01-18 01:45:46 -0300

    Adicionado o icone no layout e no login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,6 +6,7 @@
     <title>Login - SGCM</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="{{ asset('css/login.css') }}">
+    <link rel="shortcut icon" href="{{ asset('logotipo-band.ico') }}" >
 </head>
 <body>
 
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -6,6 +6,8 @@
     <title>SGCM - Sistema Gerenciador do Controle Mestre</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
+    <link rel="shortcut icon" href="{{ asset('logotipo-band.ico') }}" >
+
     <meta name="csrf-token" content="{{ csrf_token() }}">
     
     @stack('styles')
